a6886ddd7807a6a09fe544fcb21fb234122986c5
Anand H <anand.h1729@gmail.com>
2018-10-24 10:41:42 +0530

    Edited to give better results

--- captcha_breaking.py
+++ captcha_breaking.py
@@ -1,15 +1,26 @@
-from pytesseract import image_to_string 
+from pytesseract import image_to_string
 from PIL import Image
 from requests import get
 from io import BytesIO
 
-response = get("http://13.127.3.3/wp-content/uploads/image-1.png") 
- 
+#this is used to get the image from the provided link.
+response = get("http://13.127.3.3/wp-content/uploads/image-1.png")
+
+#Opens the image.
 img = Image.open(BytesIO(response.content))
 
+#Converts image to string using Pytesseract.
 text = image_to_string(img)
 
-if(text[1]=='+'): print(int(text[0])+int(text[2]))
-if(text[1]=='-'): print(int(text[0])-int(text[2]))
-if(text[1]=='*'): print(int(text[0])*int(text[2]))
-if(text[1]=='/'): print(int(text[0])/int(text[2]))
+#Prints Text.
+print(text)
+
+#Does the mathematical operations corresponding to the image.
+if (text[1]=="+"):
+    print(int(text[0])+int(text[2]))
+elif (text[1]=="-"):
+    print(int(text[0])-int(text[2]))
+elif (text[1]=="*"):
+    print(int(text[0])*int(text[2]))
+elif (text[1]=="/"):
+    print(int(text[0])/int(text[2]))
